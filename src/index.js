ee7c417920f49e36213ea44c2f047cd6ce4c079b
psychobuddha <redzistrejsn@pm.me>
2021-05-12 22:03:03 +0200

    first commit, setting up keycloak demo

--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as Keycloak from 'keycloak-js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//keycloak init options
+let initOptions = {
+  url: 'http://localhost:8180/auth',
+  realm: 'demo-realm',
+  clientId: 'react-test',
+  onLoad: 'login-required',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let keycloak = Keycloak(initOptions);
+
+keycloak
+  .init({ onLoad: initOptions.onLoad })
+  .success((auth) => {
+    if (!auth) {
+      window.location.reload();
+    } else {
+      console.info('Authenticated');
+    }
+
+    //React Render
+    ReactDOM.render(<App />, document.getElementById('root'));
+
+    localStorage.setItem('react-token', keycloak.token);
+    localStorage.setItem('react-refresh-token', keycloak.refreshToken);
+
+    setTimeout(() => {
+      keycloak
+        .updateToken(70)
+        .success((refreshed) => {
+          if (refreshed) {
+            console.debug('Token refreshed' + refreshed);
+          } else {
+            console.warn(
+              'Token not refreshed, valid for ' +
+                Math.round(
+                  keycloak.tokenParsed.exp +
+                    keycloak.timeSkew -
+                    new Date().getTime() / 1000
+                ) +
+                ' seconds'
+            );
+          }
+        })
+        .error(() => {
+          console.error('Failed to refresh token');
+        });
+    }, 60000);
+  })
+  .error(() => {
+    console.error('Authenticated Failed');
+  });
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
